653b1516d5326fc4864ffb572a89b58fb2a347b7
Richard C <richard_c@tpg.com.au>
2023-08-20 17:41:35 +1000

    Removed function dependency. Works much better.

--- chatroom.py
+++ chatroom.py
@@ -13,16 +13,15 @@ class ChatRoom:
 class SendMessageToChatRoom(BaseModel):
     message:str = Field(description='The message you wish to send')
     chatroom:str = Field(default='default', description='The chatroom for this message')
-    username:str = Field(default='anonymous', description='The name of the sender of this message')
+    #username:str = Field(default='anonymous', description='The name of the sender of this message')
 class GetRecentMessagesFromChatRoom(BaseModel):
     chatroom:str = Field(default='default', description='The chatroom to fetch messages from')
 def send_message_to_chatroom(message:str, chatroom:str='default', username:str='anonymous'):
     if chatroom not in chatrooms.keys():
         chatrooms['default'] = ChatRoom()
     chatrooms[chatroom].messages.append((username, message))
-    return get_recent_messages_from_chatroom(chatroom)
 def get_recent_messages_from_chatroom(chatroom:str='default'):
-    return "\n".join([ ": ".join(m) for m in  chatrooms[chatroom].messages[-10:]])
+    return  chatrooms[chatroom].messages[-10:]
 
 functions = [{
             "name": "SendMessageToChatRoom",
@@ -33,6 +32,8 @@ functions = [{
             "parameters": GetRecentMessagesFromChatRoom.model_json_schema()}]
 
 model='gpt-3.5-turbo'
+#openai.api_base='http://localhost:8001/v1'
+
 chatrooms:Dict = {'default': ChatRoom()}
 
 class Character:
@@ -47,62 +48,54 @@ class Character:
 
 timestamp = dt.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-# set your OpenAI API key here
-API_KEY = os.environ.get("OPENAI_API_KEY")
-
 # create a folder to store the conversations if it does not exist
 path = 'ChatGPT_conversations'
 if not os.path.exists(path):
     os.makedirs(path)
 
-    # This function creates a prompt that initializes the conversation 
 def initialize_conversation(character:Character, topic=''):
-    instructions = f' You have a conversation on {topic}. You can bring up any topic that comes to your mind'
+    """This function creates a prompt that initializes the conversation"""
+    instructions = f' You have a conversation on {topic}. You can bring up any topic that comes to your mind.'
     instructions = character.description + instructions
     task = f'Good day, Sir.'
     if topic != '':
         task = task + f' Wonderful day isn t it?'
     return instructions, task
 
-# This function creates a prompt that responds to the previous response
-def respond_prompt(character, topic=''):    
+def respond_prompt(character, topic=''):
+    '''This function creates a prompt that responds to the previous response'''
     instructions = f'You have a conversation with someone on {topic}. \
     Reply to questions and bring up any topic that comes to your mind.\
-    Dont say more than 2 sentences at a time.'
+    Dont say more than 2 sentences at a time.\
+    Use SendMessageToChatRoom to create a message.'
     instructions = character.description + instructions
     return instructions
 
-# OpenAI Engine using the turbo model
 def openai_request_initial(instructions, task, model_engine='gpt-3.5-turbo'):
+    '''OpenAI Engine using the turbo model'''
     prompt = [{"role": "system", "content": instructions }, 
               {"role": "user", "content": task }]
     #print('Generating response from OpenAI...')
     completion = openai.ChatCompletion.create(
         model=model_engine, 
         messages=prompt,
-        temperature=1.0, # this will lead to create responses that are more creative
+        temperature=1.1, # this will lead to create responses that are more creative
         max_tokens=100)
     response = completion.choices[0].message.content
     return response
 
 # OpenAI Engine using the turbo model
 def openai_request_continue(instructions, previous_messages, model_engine='gpt-3.5-turbo'):
-    prompt = [{"role": "system", "content": instructions }, 
-              #{"role": "user", "content": task }
-              ]
+    prompt = [{"role": "system", "content": instructions }]
+    for m in previous_messages:
+        prompt.append({"role": "user", "content": f"{m[0]} sent a message '{m[1]}'" })
+    prompt.append({"role": "user", "content": task })
     #print('Generating response from OpenAI...')
-    prompt.append(
-            {
-                "role": "function",
-                "name": "GetRecentMessagesFromChatRoom",
-                "content": previous_messages,
-            }
-        )  # extend conversation with function response
     completion = openai.ChatCompletion.create(
         model=model_engine, 
         messages=prompt,
-        functions=functions,
-        temperature=1.0, # this will lead to create responses that are more creative
+        #functions=functions,
+        temperature=1.1, # this will lead to create responses that are more creative
         max_tokens=100)
     response = completion.choices[0].message.content
     return response
@@ -120,10 +113,11 @@ color = 'darkblue'
 character = {
 "name": 'James (Aristocrat)',
 "description": 'You are a French nobleman from the 18th century. \
-    #Your knowledge and worldview corresponds to that of a common aristocrat of that time. \
-    #You speak in a distinguished manner and are mildly offended by profanity. \
-    #You respond in one or two sentences. \
-    #You are afraid of pirates but also curious to meet one.'}
+    Your knowledge and worldview corresponds to that of a common aristocrat of that time. \
+    You speak in a distinguished manner and are mildly offended by profanity. \
+    You respond in one or two sentences. \
+    You have a treasure but are afraid the pirate wants to steal it. \
+    You are afraid of pirates but also curious to meet one.'}
 agents.append(Character(color=color, name=character['name'], description=character['description']))
 
 # description of character 2 
@@ -136,6 +130,19 @@ character = {
     You are looking for a valuable treasure and trying to find where it is hidden. \
     You try to steer the conversation back to the treasure no matter what.'}
 agents.append(Character(color=color, name=character['name'], description=character['description']))
+
+# description of character 3
+color = 'lightorange'
+character = {
+"name": 'Bernard (Book shop owner)',
+"description": 'You are a grumpy, drunken, cynical, pessimistic, and at times depressive Irish misanthrope, whose sole pursuits in life appear to be drinking, smoking, reading, and insulting people. \
+    Your knowledge and worldview corresponds to that of an unwilling bookseller of the time. \
+    You respond in one or two sentences. \
+    You know that some of your books may contain hints about treasure. \
+    You hate both the pressures and responsibilities involved in retail, as well as your customers, with extreme passion. \
+    You try to survive each day of your blighted existence.'}
+agents.append(Character(color=color, name=character['name'], description=character['description']))
+
 
 
 conversation = ''
